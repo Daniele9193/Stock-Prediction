95047d3cdf9eea50769fffd359fbf5df72eea50d
Daniele9193 <50204835+Daniele9193@users.noreply.github.com>
2021-01-02 23:30:08 +0100

    Update Model.py

--- Model.py
+++ Model.py
@@ -68,3 +68,162 @@ def split_series(series, n_past, n_future):
     X.append(past)
     y.append(future)
   return np.array(X), np.array(y)
+
+
+n_past = 6
+n_future = 1
+n_features = 4
+
+
+X_train, y_train = split_series(train.values,n_past, n_future)
+X_train = X_train.reshape((X_train.shape[0], X_train.shape[1],n_features))
+y_train = y_train.reshape((y_train.shape[0], y_train.shape[1], n_features))
+X_val, y_val = split_series(val.values,n_past, n_future)
+X_val = X_val.reshape((X_val.shape[0], X_val.shape[1],n_features))
+y_val = y_val.reshape((y_val.shape[0], y_val.shape[1], n_features))
+X_test, y_test = split_series(test.values,n_past, n_future)
+X_test = X_test.reshape((X_test.shape[0], X_test.shape[1],n_features))
+y_test = y_test.reshape((y_test.shape[0], y_test.shape[1], n_features))
+
+
+# Model
+# E1D1
+# n_features ==> no of features at each timestep in the data.
+encoder_inputs = tf.keras.layers.Input(shape=(n_past, n_features))
+encoder_l1 = tf.keras.layers.LSTM(100, return_state=True)
+encoder_outputs1 = encoder_l1(encoder_inputs)
+
+encoder_states1 = encoder_outputs1[1:]
+
+decoder_inputs = tf.keras.layers.RepeatVector(n_future)(encoder_outputs1[0])
+
+decoder_l1 = tf.keras.layers.LSTM(100, return_sequences=True)(decoder_inputs,initial_state = encoder_states1)
+decoder_outputs1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(n_features))(decoder_l1)
+
+model_e1d1 = tf.keras.models.Model(encoder_inputs,decoder_outputs1)
+
+model_e1d1.summary()
+
+# E2D2
+# n_features ==> no of features at each timestep in the data.
+encoder_inputs = tf.keras.layers.Input(shape=(n_past, n_features))
+encoder_l1 = tf.keras.layers.LSTM(100,return_sequences = True, return_state=True)
+encoder_outputs1 = encoder_l1(encoder_inputs)
+encoder_states1 = encoder_outputs1[1:]
+encoder_l2 = tf.keras.layers.LSTM(100, return_state=True)
+encoder_outputs2 = encoder_l2(encoder_outputs1[0])
+encoder_states2 = encoder_outputs2[1:]
+
+decoder_inputs = tf.keras.layers.RepeatVector(n_future)(encoder_outputs2[0])
+
+decoder_l1 = tf.keras.layers.LSTM(100, return_sequences=True)(decoder_inputs,initial_state = encoder_states1)
+decoder_l2 = tf.keras.layers.LSTM(100, return_sequences=True)(decoder_l1,initial_state = encoder_states2)
+decoder_outputs2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(n_features))(decoder_l2)
+
+model_e2d2 = tf.keras.models.Model(encoder_inputs,decoder_outputs2)
+
+model_e2d2.summary()
+
+
+reduce_lr = tf.keras.callbacks.LearningRateScheduler(lambda x: 1e-3 * 0.90 ** x)
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss', min_delta=0, patience=0, verbose=1,
+    mode='auto', baseline=None, restore_best_weights=False
+)
+model_e1d1.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.MeanSquaredError())
+history_e1d1=model_e1d1.fit(X_train,y_train,epochs=25,validation_data=(X_test,y_test),batch_size=32,verbose=1,callbacks=[reduce_lr, early_stopping])
+model_e2d2.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.MeanSquaredError())
+history_e2d2=model_e2d2.fit(X_train,y_train,epochs=25,validation_data=(X_val,y_val),batch_size=32,verbose=1,callbacks=[reduce_lr, early_stopping])
+
+
+plt.plot(history_e1d1.history['loss'])
+plt.plot(history_e1d1.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+
+
+plt.plot(history_e2d2.history['loss'])
+plt.plot(history_e2d2.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+
+
+pred_e2d2=model_e2d2.predict(X_test)
+
+print(pred_e2d2.shape)
+
+reduce_lr = tf.keras.callbacks.LearningRateScheduler(lambda x: 1e-3 * 0.90 ** x)
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss', min_delta=0, patience=0, verbose=1,
+    mode='auto', baseline=None, restore_best_weights=False
+)
+
+model_lstm = tf.keras.models.Sequential([
+    tf.keras.layers.Input(shape=(n_past, n_features)),
+    # Shape [batch, time, features] => [batch, time, lstm_units]
+    tf.keras.layers.LSTM(64, return_sequences=False),
+    # Shape => [batch, time, features]
+    tf.keras.layers.Dense(units=4),
+    #tf.keras.layers.Reshape([1, -1])
+])
+
+model_lstm.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.MeanSquaredError())
+history_lstm=model_lstm.fit(X_train,y_train,epochs=100,validation_data=(X_val,y_val),batch_size=32,verbose=1,callbacks=[reduce_lr, early_stopping])
+
+plt.plot(history_lstm.history['loss'])
+plt.plot(history_lstm.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+
+pred_lstm=model_lstm.predict(X_test)
+print(X_test.shape)
+print(y_test.shape)
+
+print(pred_lstm)
+print(pred_lstm.shape)
+
+len(pred_lstm)
+len(y_test)
+
+X_prova = X_test
+
+X_plot = np.concatenate([X_prova[0],[pred_lstm[0]]])
+print(X_plot)
+
+plot = []
+for i in X_plot:
+    plot.append(i[0])
+
+plt.figure()
+plt.scatter(x=list(range(0,len(plot))), y=plot)
+plt.show()
+
+
+
+X_true = X_test
+X_true = np.concatenate([X_true[0],y_test[0]])
+print(X_true)
+
+plot = []
+for i in X_true:
+    plot.append(i[0])
+
+plt.figure()
+plt.scatter(x=list(range(0,len(plot))), y=plot)
+plt.show()
+
+
+for index,i in enumerate(train_df.columns):
+    scaler = scalers['scaler_'+i]
+    pred_lstm[:,:,index]=scaler.inverse_transform(pred_lstm[:,:,index])
+    y_train[:,:,index]=scaler.inverse_transform(y_train[:,:,index])
+    y_test[:,:,index]=scaler.inverse_transform(y_test[:,:,index])
